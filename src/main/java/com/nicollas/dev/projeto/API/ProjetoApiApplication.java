98897ca23e7663f2d052f7cd2994e5afab2f1eb6
Nicollas <nsilvademoraes@gmail.com>
2024-06-16 16:22:28 -0300

    Iniciando Projeto e Configurando Pasta de Banco de Dados

---
+++ src/main/java/com/nicollas/dev/projeto/API/ProjetoApiApplication.java
@@ -0,0 +1,13 @@
+package com.nicollas.dev.projeto.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiApplication.class, args);
+	}
+
+}
